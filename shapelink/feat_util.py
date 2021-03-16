8d55d424dbe890dcdb38d1b8adb520eee7a69ff2
Eoghan O'Connell <eoclives@hotmail.com>
2021-03-16 18:25:32 +0100

    tests: added a choose feature test including the trace feature

--- shapelink/feat_util.py
+++ shapelink/feat_util.py
@@ -3,7 +3,7 @@ import dclab.definitions as dfn
 
 
 def map_requested_features_to_defined_features(features):
-    '''
+    """
     Map the flat list from plugin to list of 3 lists: sc, tr, im
 
     For example:
@@ -16,7 +16,7 @@ def map_requested_features_to_defined_features(features):
         A flat list of strings which is mapped to features as defined
         in the `dclab.definitions` module.
 
-    '''
+    """
 
     sc_features, tr_features, im_features = [], [], []
     for feat in features:
--- tests/test_feature_mapping.py
+++ tests/test_feature_mapping.py
@@ -52,8 +52,8 @@ def test_incorrect_feature_spelling():
 
 
 def test_single_fluor_trace_feature():
-    '''Check that a FLUOR_TRACES feature name is correctly mapped to the
-    traces list. '''
+    """Check that a FLUOR_TRACES feature name is correctly mapped to the
+    traces list. """
     user_feats = ["fl1_median"]
     feats = map_requested_features_to_defined_features(user_feats)
     sc, tr, im = feats
@@ -61,8 +61,8 @@ def test_single_fluor_trace_feature():
 
 
 def test_fluor_trace_feature_with_slash():
-    '''Check that a FLUOR_TRACES feature name with the "trace/" prefix
-    is correctly mapped to the traces list. '''
+    """Check that a FLUOR_TRACES feature name with the "trace/" prefix
+    is correctly mapped to the traces list. """
     user_feats = ["trace/fl1_median"]
     feats = map_requested_features_to_defined_features(user_feats)
     sc, tr, im = feats
@@ -70,8 +70,8 @@ def test_fluor_trace_feature_with_slash():
 
 
 def test_mix_fluor_traces_input():
-    '''Check that a FLUOR_TRACES feature name with the "trace/" prefix
-    is correctly mapped to the traces list. '''
+    """Check that a FLUOR_TRACES feature name with the "trace/" prefix
+    is correctly mapped to the traces list. """
     user_feats = ["trace/fl1_median", "fl2_raw"]
     feats = map_requested_features_to_defined_features(user_feats)
     sc, tr, im = feats
@@ -79,8 +79,8 @@ def test_mix_fluor_traces_input():
 
 
 def test_all_fluor_trace_feature():
-    '''Check that "trace" correctly returns all FLUOR_TRACES to the
-    traces list. '''
+    """Check that "trace" correctly returns all FLUOR_TRACES to the
+    traces list. """
     user_feats = ["trace"]
     feats = map_requested_features_to_defined_features(user_feats)
     sc, tr, im = feats
