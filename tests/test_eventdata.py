02faba11a66cb8c3220a475ba7f7b82296046903
Eoghan O'Connell <eoclives@hotmail.com>
2021-03-17 09:48:25 +0100

    ref: cleaned up docstring

--- tests/test_eventdata.py
+++ tests/test_eventdata.py
@@ -3,7 +3,6 @@ from shapelink.shapelink_plugin import EventData
 
 
 def test_eventdata_attribute_types(EventData=EventData):
-
     e = EventData()
     assert isinstance(e.id, int), "Should be an int"
     assert isinstance(e.scalars, list), "Should be a list"
